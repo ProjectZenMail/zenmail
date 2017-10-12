95ae8e6d9e2c849311691c6d877edaaf8a11f9bc
Andrew Shipilo <superdron97@yandex.ru>
2017-10-12 16:07:26 +0300

    User model, main controller

--- src/main/java/org/zen/zenmail/ZenmailApplication.java
+++ src/main/java/org/zen/zenmail/ZenmailApplication.java
@@ -2,11 +2,16 @@ package org.zen.zenmail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"org.zen.zenmail.repository"})
+@EntityScan(basePackages = {"org.zen.zenmail.model"})
+@EnableTransactionManagement
 public class ZenmailApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ZenmailApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZenmailApplication.class, args);
+    }
 }
---
+++ src/main/java/org/zen/zenmail/model/VersionModel.java
@@ -0,0 +1,11 @@
+package org.zen.zenmail.model;
+
+import lombok.Data;
+
+@Data  // For Getters and Setters
+public class VersionModel {
+    private String version = null;
+    private Integer major = null;
+    private Integer minor = null;
+    private Integer patch = null;
+}

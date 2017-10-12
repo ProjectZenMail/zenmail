2e9eca79c146d74a8ac295497984c4a903d4829b
Andrew Shipilo <superdron97@yandex.ru>
2017-10-12 15:03:46 +0300

    Base springboot app

---
+++ src/main/java/org/zen/zenmail/ZenmailApplication.java
@@ -0,0 +1,12 @@
+package org.zen.zenmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenmailApplication.class, args);
+	}
+}

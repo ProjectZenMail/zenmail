335a44cf7e7aa9fd6c3a507ab880a3dd3686c40f
Andrew Shipilo <superdron97@yandex.ru>
2017-10-12 16:20:55 +0300

    Spring security token auth

---
+++ src/main/java/org/zen/zenmail/identity/TokenUser.java
@@ -0,0 +1,23 @@
+package org.zen.zenmail.identity;
+
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.zen.zenmail.model.user.User;
+
+public class TokenUser extends org.springframework.security.core.userdetails.User {
+    private User user;
+
+    //For returning a normal user
+    public TokenUser(User user) {
+        super(user.getUserId(), user.getPassword(), AuthorityUtils.createAuthorityList(user.getRole().toString()));
+        //super(user.getUserName(), user.getPassword(), true, true, true, true,  AuthorityUtils.createAuthorityList(user.getRole().toString()));
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getRole() {
+        return user.getRole().toString();
+    }
+}

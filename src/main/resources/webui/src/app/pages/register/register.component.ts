c599c488f9c829147b50159afe1572088136ea56
Artem Zabolotnyi <zaaabik@gmail.com>
2017-10-18 21:20:23 +0300

    added register method  for adding user to mail user database

--- src/main/resources/webui/src/app/pages/register/register.component.ts
+++ src/main/resources/webui/src/app/pages/register/register.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {LoginService} from '../../services/api/login.service';
 import {Router} from '@angular/router';
+import {RegisterService} from "../../services/api/register.service";
 
 @Component({
     templateUrl: './register.component.html',
@@ -12,14 +13,18 @@ export class RegisterComponent implements OnInit {
     errMsg: string = '';
 
     constructor(private router: Router,
-                private loginService: LoginService) {
+                private registerService: RegisterService) {
     }
 
     ngOnInit() {
     }
 
     register() {
+        let success = false;
+        success = this.registerService.register(this.model.name,this.model.email,this.model.password);
+
     }
+
 
 
 }

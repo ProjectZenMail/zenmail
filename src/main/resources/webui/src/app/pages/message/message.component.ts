a40ef0e957547e9268ffe1013a1b7a5be6fd3a6c
Artem Zabolotnyi <zaaabik@gmail.com>
2017-12-07 00:56:15 +0300

    full message showing

--- src/main/resources/webui/src/app/pages/message/message.component.ts
+++ src/main/resources/webui/src/app/pages/message/message.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {DataStorageService} from "../../services/data-storege.service";
+import {MessageService} from "../../services/api/message.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-message',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message.component.scss']
 })
 export class MessageComponent implements OnInit {
-
-  constructor() { }
+  message:any;
+  constructor(private messageService: MessageService,private dataStorage : DataStorageService, private  route : Router) {
+  }
 
   ngOnInit() {
+      let msg : any;
+      this.message = {
+          "title":"",
+          "id":"",
+          "sender" : "",
+          "date" : "",
+          "body" : "",
+      }
+
+    if(this.dataStorage.isEmptyOneMsg()){
+        this.messageService.getMessageById(this.route.url.split('/').pop())
+            .subscribe(res => {
+                    msg = res.msgs[0];
+                    console.log(this.route.url);
+                    this.dataStorage.setData(msg);
+                    this.message = {
+                        "title":msg.subject,
+                        "id":msg.id,
+                        "sender" : msg.from,
+                        "date" : msg.time,
+                        "body" : msg.msg,
+                    }
+                    this.dataStorage.setData(this.message);
+                }
+            )
+    }else {
+
+        this.message = this.dataStorage.getData();
+    }
   }
 
 }
